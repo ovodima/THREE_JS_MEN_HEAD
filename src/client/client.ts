7ac28c537ee6246636381fdbd80d4837a74bd96e
Dima <dima.tsymbal12345@gmail.com>
2023-02-03 20:22:33 +0200

    three head

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,21 +1,24 @@
 import * as THREE from 'three'
-import { AmbientLight, Color, Mesh, MeshStandardMaterial } from "three";
+import { AmbientLight, PointLight, Color, Mesh, MeshStandardMaterial, ObjectLoader } from "three";
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+const scene: THREE.Scene = new THREE.Scene()
 
-const scene = new THREE.Scene()
-
-const camera = new THREE.PerspectiveCamera(
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
-camera.position.z = 20
+camera.position.z = 3.5
+// camera.position.y = 2
+
+
 
 const canvas = document.querySelector('.webgl') as HTMLBodyElement
-const renderer = new THREE.WebGLRenderer({canvas})
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-const loadingManager = new THREE.LoadingManager();
+const loadingManager: THREE.LoadingManager = new THREE.LoadingManager();
 
 loadingManager.onStart = () => {
   console.log("onstart");
@@ -27,77 +30,92 @@ loadingManager.onError = () => {
   console.log("onerror");
 };
 
+const ambLight: THREE.DirectionalLight = new THREE.DirectionalLight()
+
+ambLight.position.x = 2.5
+ambLight.intensity = 1.2
+
 function textures(link: string) {
     return new THREE.TextureLoader(loadingManager).load(link);
 }
 
+
 const agateColor = textures("textures/Agate_001_COLOR.jpg");
-const agateRough = textures("textures/Agate_001_ROUGH.jpg");
-const agateOCC = textures("textures/Agate_001_OCC.jpg");
-const agateNORM = textures("textures/Agate_001_NORM.jpg");
-const agateDisp = textures("textures/Agate_001_DISP.jpg");
+// const agateDisp = textures("textures/Agate_001_DISP.jpg");
+// const agateNORM = textures("textures/Agate_001_NORM.jpg");
+const agateROUGH = textures("textures/Agate_001_ROUGH.jpg");
+// const agateOCC = textures("textures/Agate_001_OCC.jpg");
+
+const metalColor = textures("textures/metal/Material_2079.jpg");
+const organicDISP = textures("textures/metal/Organic_abstract_001_DISP.jpg");
+const organicNORM = textures("textures/metal/Organic_abstract_001_NORM.jpg");
+const organicOCC = textures("textures/metal/Organic_abstract_001_OCC.jpg");
+const metalROUGH = textures("textures/metal/Metal_006_roughness.jpg");
 
 
-function textureOption(texture: any) {
-    texture.anisotropy = 2;
-  
-    texture.minFilter = THREE.NearestMipMapNearestFilter;
-    texture.minFilter = THREE.NearestMipMapNearestFilter;
-  
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-  
-    // texture.repeat.set(4, 4);
-  }
-
-  textureOption(agateColor)
-//   textureOption(agateRough)
+const materialPh: THREE.MeshPhysicalMaterial = new THREE.MeshPhysicalMaterial({
+    map:agateColor,
+    clearcoatRoughnessMap: agateROUGH,
+})
 
 
-const geometrys = [
-    new THREE.SphereGeometry( 10, 32, 16 ),
-    new THREE.TorusGeometry( 5, 3, 16, 100 )
-]
+// const material: THREE.MeshMatcapMaterial = new THREE.MeshMatcapMaterial({
+//     // map:organicColor,
+//     displacementMap:organicDISP,
+//     bumpMap:organicOCC,
+//     normalMap:organicNORM,
+//     alphaMap:organicROUGH,
+//     matcap:organicColor,
+// })
 
-const materials = [
-    new THREE.MeshPhongMaterial({
-        color:0x00EAB71D
-    }),
-    new THREE.MeshMatcapMaterial({
-        // alphaMap:agateRough,
-        // bumpMap:agateColor,
-        // displacementMap:agateDisp,
-        // map:agateOCC,
-        // normalMap:agateNORM,
+const objLoader: OBJLoader = new OBJLoader()
+objLoader.load(
+    'models/Head 2 from Heads Bundle 2 decimated.obj',
+    (object) => {
+        // (object.children[0] as THREE.Mesh).material = material
+        object.rotation.y = 0.7
+
+      object.position.y = 1
+
+      window.addEventListener('mousemove', (e) => {
+        // let pageX = Math.round(e.pageX / 1000)
+        // let pageY = Math.round(e.pageY / 1000)
+
+        let pageX = Math.round(e.pageX) / 10002
+        let pageY = e.pageY / 10002
+
+        // object.rotation.x += pageX / 10
+        object.rotation.y += pageY
         
-        alphaTest: 0.1,
-        matcap:agateColor,
-        alphaMap:agateRough,
-        displacementMap:agateDisp,
-        normalMap:agateOCC,
-        bumpMap: agateNORM
+
+        camera.position.x = 0.0005
+        camera.position.y = pageX
+
+      })
+
+
   
-
         
-        
-    }),
-    new THREE.MeshPhysicalMaterial(),
-]
+        object.traverse(function (child) {
+            if ((child as THREE.Mesh).isMesh) {
+                (child as THREE.Mesh).material = materialPh
+            }
+        })
+        scene.add(object, )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
-const light: THREE.DirectionalLight = new THREE.DirectionalLight(0x404040, 2)
-
-const lighting = [
-     new THREE.AmbientLight(),
-     new THREE.DirectionalLight(0xffffff, 5),
-     new THREE.PointLight(),
-     new THREE.SpotLight(),
-];
 
 
 
 
-const torus = new THREE.Mesh(geometrys[0], materials[1])
-scene.add(torus)
+scene.add(ambLight)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -110,8 +128,8 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    torus.rotation.x += 0.002
-    torus.rotation.y += 0.002
+    // torus.rotation.x += 0.002
+    // torus.rotation.y += 0.002
 
     render()
 }
